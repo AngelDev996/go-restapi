d7af782668f906369adcd4c0b3e777cce04d483c
Angel <arivalladares2.0@gmail.com>
2022-07-01 02:50:14 -0600

    handlers

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
+// mi 23:49
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -29,10 +32,36 @@ func indexRoute(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func getTask(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(tasks)
+
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var newTask task
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Insert a valid task")
+	}
+
+	json.Unmarshal(reqBody, &newTask)
+
+	newTask.ID = len(tasks) + 1
+
+	tasks = append(tasks, newTask)
+
+	w.Header().Set("Conten-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+
+}
+
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", indexRoute)
+	router.HandleFunc("/tasks", getTask).Methods("GET")
 	http.ListenAndServe(":3000", router)
 	log.Fatal(http.ListenAndServe(":3000", router))
+
 }
