37005ccc9fb22d509de6a939bf270f3324bb5714
Angel <arivalladares2.0@gmail.com>
2022-06-30 22:47:41 -0600

    go mux added

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+type task struct {
+	ID      int    `json:ID`
+	Name    string `json:Name`
+	Content string `json:Content`
+}
+
+type allTask []task
+
+var tasks = allTask{
+	{
+		ID: 1,
+		Name: "Task One",
+		Content: "Some Content"
+	}
+}
+
+
 func main() {
 
 	fmt.Println("Hola mundo")
